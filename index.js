bba61bb494f71a32b3a2e3a1e40a5d38d0dddcce
try things <trythings3@gmail.com>
2022-12-15 21:38:23 +0200

    keyboard input

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ import GameBoard from "./src/gameBoard.js";
 import Player from "./src/player.js";
 import Point from "./src/point.js";
 import Monster from "./src/monster.js";
+import readline from "node:readline";
 
 const b = new GameBoard();
-b.printBoard();
+
+// console.log(readline);
+// readline.emitKeypressEvents(process.stdin.on("data", (d) => console.log(d)));
+process.stdin.on("data", (d) => {
+    console.log(d)
+    b.printBoard();
+
+    // process.exit();
+})
+// while (true) {
+    
+    // readline.emitKeypressEvents(process.stdin.on("data", (d) => console.log(d)));
+    // if (process.stdin.isTTY) process.stdin.setRawMode(true);
+    // process.stdin.on("data", data => {
+        // console.log(data);
+        // 
+    // });
+// }
 // b.setPlayerLocation(0,1);
