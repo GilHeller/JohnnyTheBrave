77df85494b26ac6a033637206592b2748c879600
anaellelahiany <909anaelle@gmail.com>
2022-12-15 20:54:52 +0200

    .

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ import GameBoard from "./src/gameBoard.js";
 import Player from "./src/player.js";
 import Point from "./src/point.js";
 import Monster from "./src/monster.js";
+import readline from"node:readline";
 
 const b = new GameBoard();
+//console.log(readline);
+//readline.emitKeypresssEvents(process.stdin);
+process.stdin.on("data",(d)=>{
+    console.log(d)
+    b.printBoard();
+
+    //process.exsit()
+})
 b.printBoard()
-// b.setPlayerLocation(0,1);
+const moveUp =()=>{
+}
+ process.stdin
+const moveDown =()=>{
+    
+}
+const moveRight =()=>{
+    
+}
+const move =(sometask)=>{
+    if (!sometask) {
+        if (keycode == 37 || keycode == 65) {
+            this.location.setPoint();
+        }
+        if (keycode == 38 || keycode == 87) {
+            this.location.setPoint();
+        }
+        if (keycode == 39 || keycode == 68) {
+            this.location.setPoint();
+        }
+        if (keycode == 40 || keycode == 83) {
+            this.location.setPoint();
+        }
+      }
+}
+
+ 
+//this.init = function() {
+    //keys = {};
+
+//{
+// this.trgt = ()=>{
+    //if(this.event.target === myContainer.querySelector('#start')) {
+       //myModel.startGame();
+       //window.addEventListener('keydown', self.keyDown);
+       //window.addEventListener('keyup', self.keyUp);
+    //}
+   
+ //}
